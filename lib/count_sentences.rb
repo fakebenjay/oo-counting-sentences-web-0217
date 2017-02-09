1de16b6af5a1bd4fc9c629736e27c1e7f5e044ce
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-08 22:33:19 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    array.delete("")
+    array.count
   end
 end
